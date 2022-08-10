ba363f22a16c92fb1da5013966ec8ab4241a3074
Dhiwakar Kusuma <dhiwakark@github.com>
2022-08-10 10:44:20 +0000

    asdf

--- index.js
+++ index.js
@@ -13,7 +13,19 @@ async function run() {
         owner: owner,
         repo: repo,
       });
+    const artifacts = getArtifactsForRepo.data.artifacts;
+    console.log('Artifact Count');
+    console.log(artifacts.length);
 
+    var size = 0;
+    for (let i = 0; i < artifacts.length; i++) {
+        console.log('Artifact Name');
+        console.log(artifacts[i].name);
+        console.log('Artifact Size');
+        console.log(artifacts[i].size_in_bytes);
+        size += artifacts[i].size_in_bytes / 1000000.0;
+    }
+    console.log('Total Size of Artifacts' + size + ' MB');
   } catch (error) {
     core.setFailed(error.message);
   }
