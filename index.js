de79f25e4b1fbd83028f676f4f6adc63c58ec888
Dhiwakar Kusuma <dhiwakark@github.com>
2022-08-10 09:15:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  const getArtifactsForRepo = await github.rest.actions.listArtifactsForRepo({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-    });
-  const artifacts = getArtifactsForRepo.data.artifacts;
-  console.log('Artifact Count');
-  console.log(artifacts.length);
+async function run() {
+  try {
+    const getArtifactsForRepo = await github.rest.actions.listArtifactsForRepo({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+      });
+    const artifacts = getArtifactsForRepo.data.artifacts;
+    console.log('Artifact Count');
+    console.log(artifacts.length);
 
-  var size = 0;
-  for (let i = 0; i < artifacts.length; i++) {
-      console.log('Artifact Name');
-      console.log(artifacts[i].name);
-      console.log('Artifact Size');
-      console.log(artifacts[i].size_in_bytes);
-      size += artifacts[i].size_in_bytes / 1000000.0;
+    var size = 0;
+    for (let i = 0; i < artifacts.length; i++) {
+        console.log('Artifact Name');
+        console.log(artifacts[i].name);
+        console.log('Artifact Size');
+        console.log(artifacts[i].size_in_bytes);
+        size += artifacts[i].size_in_bytes / 1000000.0;
+    }
+    console.log('Total Size of Artifacts' + size + ' MB');
+  } catch (error) {
+    core.setFailed(error.message);
   }
-  console.log('Total Size of Artifacts' + size + ' MB');
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run();
